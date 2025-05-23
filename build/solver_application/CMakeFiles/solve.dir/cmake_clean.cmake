54800ef28faee43cd9912e9aff4e99e80f26bd46
alexvsl2007 <alexvsl2007@gmail.com~>
2025-05-23 21:00:50 +0300

    update

---
+++ build/solver_application/CMakeFiles/solve.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solve.dir/link.d"
+  "CMakeFiles/solve.dir/equation.cpp.o"
+  "CMakeFiles/solve.dir/equation.cpp.o.d"
+  "solve"
+  "solve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
