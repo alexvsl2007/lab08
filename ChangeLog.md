082221cbb54e13f0801e68a74e4ddf02431ce327
alexvsl2007 <alexvsl2007@gmail.com~>
2025-05-24 19:12:31 +0300

    Added CPack support for RPM, DEB, TGZ

---
+++ ChangeLog.md
@@ -0,0 +1,2 @@
+* Sat May 24 2025 alexvsl2007 <alexvsl2007@gmail.com> 0.1.0.0
+- Initial RPM release
---
+++ _build/CMakeFiles/RunTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunTest.dir/link.d"
+  "CMakeFiles/RunTest.dir/test.cpp.o"
+  "CMakeFiles/RunTest.dir/test.cpp.o.d"
+  "RunTest"
+  "RunTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RunTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
