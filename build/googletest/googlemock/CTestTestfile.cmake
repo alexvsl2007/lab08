9714ff50f0e3c0179d1ff44482a3e6c2ad3616e2
alexvsl2007 <alexvsl2007@gmail.com~>
2025-05-24 20:05:33 +0300

    update

---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mordecai/workspace/projects/lab06/googletest/googlemock
+# Build directory: /home/mordecai/workspace/projects/lab06/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mordecai/workspace/projects/lab06/googletest/googletest
+# Build directory: /home/mordecai/workspace/projects/lab06/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
